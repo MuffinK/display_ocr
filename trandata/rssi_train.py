16aba6d91b7fac9089d6de18f98c8131722ab9d9
金潇泽 <jinxz@sdc.icbc.com.cn>
2017-11-22 17:17:37 +0800

    rssi

--- trandata/rssi_train.py
+++ trandata/rssi_train.py
@@ -6,96 +6,107 @@ import numpy as np
 import json
 import PIL.ImageDraw as ImageDraw
 import PIL.Image as Image
-import PIL.ImageShow as ImageShow
 import math
 
-im = Image.new("RGBA", (1500, 1000), (255, 255, 255, 0))
+def draw_line ():
+    im = Image.new("RGBA", (1500, 1000), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(im)
+    for i in range(len(x_data)):
+        if device[i] == '4':
+            color = (255, 0, 0)
+        elif device[i] == '2':
+            color = (0, 255, 0)
+        elif device[i] == '3':
+            color = (0, 0, 255)
+        else:
+            pass
+        draw.point((x_data[i] * 200 + 3 * np.random.randn(), (y_data[i]) * -10 + 3 * np.random.randn()), fill=color)
+    trained_x = [i for i in range(1, 1500)]
+    trained_y = np.array((np.log10(np.array(trained_x) / 200.0)
+                          * sess[deviceId].run(W[deviceId]) + sess[deviceId].run(p0[deviceId])) * -10).tolist()
 
-p0 = {
-    "2": tf.Variable(tf.zeros([1]) + 10),
-    "3": tf.Variable(tf.zeros([1]) + 10),
-    "4": tf.Variable(tf.zeros([1]) + 10)
-}
-W = {
-    "2": tf.Variable(tf.zeros([1])),
-    "3": tf.Variable(tf.zeros([1])),
-    "4": tf.Variable(tf.zeros([1]))
-}
+    for i in range(len(trained_x)):
+        draw.point((trained_x[i], trained_y[i]), fill=color)
 
-# 初始化变量
-init = tf.global_variables_initializer()
+    im.show()
 
-# 启动图 (graph)
-sess = {
-    "2": tf.Session(),
-    "3": tf.Session(),
-    "4": tf.Session()
-}
-[sess[i].run(init) for i in sess]
+def get_tran_data():
+    x_data = []
+    y_data = []
+    for i in range(1,31,1):
+        x_data.append([])
+        y_data.append([])
+    with open('./trandata/rssi') as data_file:
+        data = json.load(data_file)
+    for minor in data:
+        distance = 0
+        times = 0
+        for item in data[minor]:
+            if distance == item['distance'] and times >= 100:
+                continue
+            if distance != item['distance']:
+                times = 0
+            distance = item['distance']
+            times = times + 1
+            y_data[int(minor)-1].append(float(item['distance']))
+            x_data[int(minor)-1].append(float(item['rssi'])/-10)
 
-saver = tf.train.Saver()
-
-with open('./trandata/data.json') as data_file:
-    data = json.load(data_file)
-for deviceId in ['2', '3', '4']:
-
-        x_data = []
-        y_data = []
-        device = []
-        for obj in data:
-            if str(obj['deviceId']) == deviceId:
-                x_data.append(float(obj['distance']))
-                y_data.append(float(obj['rssi']))
-                device.append(str(obj['deviceId']))
-
-        np_x_data = np.array(x_data).astype(np.float32)
-        np_y_data = np.array(y_data).astype(np.float32)
+    return (np.array(x_data), np.array(y_data))
 
 
-        def log10(x):
-            numerator = tf.log(x)
-            denominator = tf.log(tf.constant(10, dtype=numerator.dtype))
-            return numerator / denominator
 
-        y = W[deviceId] * log10(x_data) + p0[deviceId]
+def tran() :
+    p = tf.Variable(tf.zeros([1]) + 1)
+    W = tf.Variable(tf.zeros([1]))
 
-        # 最小化方差
-        loss = tf.reduce_mean(tf.square(y - np_y_data))
+    (x_data, y_data) = get_tran_data()
+    # print (x_data, y_data)
+    np_x_data = np.array(y_data).astype(np.float32).transpose()
+    np_y_data = np.array(x_data).astype(np.float32).transpose()
+    def log10(x):
+        numerator = tf.log(x)
+        denominator = tf.log(tf.constant(10, dtype=numerator.dtype))
+        return numerator / denominator
+
+    # 最小化方差
+
+
+
+    for i in range(1, 31, 1):
+        y = log10(np_x_data[i]) * W + p
+        loss = tf.reduce_mean(tf.square(y - np_y_data[i]))
         optimizer = tf.train.GradientDescentOptimizer(0.5)
         train = optimizer.minimize(loss)
 
-        # 拟合平面
-        for step in xrange(0, 200):
-            sess[deviceId].run(train)
-            print step, sess[deviceId].run(W[deviceId]), sess[deviceId].run(p0[deviceId])
+        # 初始化变量
+        init = tf.global_variables_initializer()
 
-        save_path = saver.save(sess[deviceId], "./trandata/model.ckpt" + deviceId)
+        sess = tf.Session()
+        sess.run(init)
 
-        draw = ImageDraw.Draw(im)
-        for i in range(len(x_data)):
-            if device[i] == '4':
-                color = (255, 0, 0)
-            elif device[i] == '2':
-                color = (0, 255, 0)
-            elif device[i] == '3':
-                color = (0, 0, 255)
-            else:
-                pass
-            draw.point((x_data[i] * 200 + 3 * np.random.randn(), (y_data[i]) * -10 + 3 * np.random.randn()), fill=color)
+        saver = tf.train.Saver()
+        # sess.reset()
+        # tf.Session.close(sess)
 
-        trained_x = [i for i in range(1, 1500)]
-        trained_y = np.array((np.log10(np.array(trained_x) / 200.0)
-                              * sess[deviceId].run(W[deviceId]) + sess[deviceId].run(p0[deviceId])) * -10).tolist()
 
-        for i in range(len(trained_x)):
-            draw.point((trained_x[i], trained_y[i]), fill=color)
+        # # 拟合平面
+        for step in xrange(0, 500):
+            if step % 20 == 0:
+                print step, sess.run(W), sess.run(p)
+            sess.run(train)
 
-# im.show()
+        # traned_w = sess.run(W)
+        # traned_p = sess.run(p)
+
+    save_path = saver.save(sess, "./trandata/model.ckpt")
+    return (traned_w, traned_p)
 
 
 def getDistance(rssi_value, w, p):
     return np.power(10.0, (np.array([rssi_value]) - p) / w).tolist()
 
+
+tran()
 
 from flask import Flask, request
 import json
@@ -104,20 +115,20 @@ app = Flask(__name__)
 x0 = [0, 5, 0]
 y0 = [0, 0, 5]
 
-def getXY(pointA, pointB, dA, dB):
-    x = 0
-    y = 0
-    if pointA.x == pointB.x:
-        y = pointA.y
-        x = y
-    elif pointA.y == pointB.y:
-        x = pointA.x
-        y = pointB.x
-    else:
-        x = pointA.x
-        y = pointB.y
-
-    return (x, y)
+# def getXY(pointA, pointB, dA, dB):
+#     x = 0
+#     y = 0
+#     if pointA.x == pointB.x:
+#         y = pointA.y
+#         x = y
+#     elif pointA.y == pointB.y:
+#         x = pointA.x
+#         y = pointB.x
+#     else:
+#         x = pointA.x
+#         y = pointB.y
+#
+#     return (x, y)
 
 
 def sq(x):
@@ -192,7 +203,7 @@ def hello_world():
                         get_point((5.0, 0.0), (0.0, 0.0), d3, d1)[1],
                         d1, d2, d3)
     print center
-    if center == -1 :
+    if center == -1:
         return ('', 500)
     else:
         return (json.dumps({
